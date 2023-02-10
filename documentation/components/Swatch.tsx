820cef3b9cd1f895d15a51c113e86cd7d40686f0
Jared White <jared@jaredwhite.com>
2023-02-10 14:23:39 -0800

    remove: Tailwind

--- documentation/components/Swatch.tsx
+++ documentation/components/Swatch.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React from "react"
 
-export const Swatch = (props: { color: string, border?: boolean, label?: string }) => (
-  <div className="inline-block text-center">
+export const Swatch = (props: { color: string; border?: boolean; label?: string }) => (
+  <div style={{ display: "inline-block", textAlign: "center" }}>
     <span
-      style={{ backgroundColor: `var(--${props.color})` }}
+      style={{
+        backgroundColor: `var(--${props.color})`,
+        width: "var(--bloom-s6)",
+        borderRadius: "var(--bloom-rounded-sm)",
+        display: "block",
+        borderWidth: "1px",
+        borderStyle: "solid",
+        borderColor: props.border ? "var(--bloom-color-gray-500)" : "transparent",
+      }}
       title={props.color}
-      className={`w-9 rounded-sm block ${props.border ? "border border-gray-500" : ""}`}
     >
       &nbsp;
     </span>
     {props.label && <span>{props.label}</span>}
   </div>
-);
+)
