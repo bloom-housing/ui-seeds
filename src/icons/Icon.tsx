8a50920c26f26bb57f6396b7010857de46a20364
Jared White <jared@jaredwhite.com>
2023-02-10 13:39:56 -0800

    feat: first draft of the Icon component

---
+++ src/icons/Icon.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Icon, IconDefinition } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+import "./Icon.scss"
+
+export interface IconProps {
+  icon: IconDefinition
+  id?: string
+  className?: string 
+  ariaHidden?: boolean
+}
+
+const Icon = (props: IconProps) => {
+  const classNames = ["ui-icon"]
+  if (props.className) classNames.push(props.className)
+
+  return (
+    <span id={props.id} className={classNames.join(" ")} aria-hidden={props.ariaHidden}>
+      <FontAwesomeIcon icon={props.icon} />
+    </span>
+  )
+}
+
+export default Icon
