407dcb81702586f1fb1ed681e530c98354c3d738
Jared White <jared@jaredwhite.com>
2023-03-26 14:59:09 -0700

    docs: simplify Toast docs

--- src/blocks/__stories__/Toast.stories.tsx
+++ src/blocks/__stories__/Toast.stories.tsx
@@ -22,23 +22,23 @@ export const toasts = () => (
 )
 
 export const standardVariants = () => (
-  <div style={{ display: "grid", gap: "var(--bloom-s3)" }}>
+  <>
     <Toast variant="primary">Primary toast</Toast>
     <Toast variant="success">Success toast</Toast>
     <Toast variant="warn">Warning toast</Toast>
     <Toast variant="alert">Alert toast</Toast>
     <Toast variant="secondary">Secondary toast</Toast>
-  </div>
+  </>
 )
 
 export const inverseVariants = () => (
-  <div style={{ display: "grid", gap: "var(--bloom-s3)" }}>
+  <>
     <Toast variant="primary-inverse">Primary toast</Toast>
     <Toast variant="success-inverse">Success toast</Toast>
     <Toast variant="warn-inverse">Warning toast</Toast>
     <Toast variant="alert-inverse">Alert toast</Toast>
     <Toast variant="secondary-inverse">Secondary toast</Toast>
-  </div>
+  </>
 )
 
 export const showToasts = () => {
@@ -47,12 +47,23 @@ export const showToasts = () => {
 
   return (
     <>
-      <button onClick={() => showSuccess(!success)} style={{ background: "#f0f0f0" }}>
-        {success ? "Remove" : "Show"} Success Toast
-      </button>{" "}
-      <button onClick={() => showWarning(!warning)} style={{ background: "#f0f0f0" }}>{warning ? "Remove" : "Show"} Warn Toast</button>
-      {success && <Toast variant="success" hideTimeout={5000}>Success message!</Toast>}
-      {warning && <Toast variant="warn" hideTimeout={5000}>Warn message!</Toast>}
+      <label>
+        <input type="checkbox" onChange={() => showSuccess(!success)} /> Success Toast
+      </label>{" "}
+      <label>
+        <input type="checkbox" onClick={() => showWarning(!warning)} /> Warn Toast
+      </label>
+      <p>(they'll disappear after a few seconds)</p>
+      {success && (
+        <Toast variant="success" hideTimeout={5000}>
+          Success message!
+        </Toast>
+      )}
+      {warning && (
+        <Toast variant="warn" hideTimeout={5000}>
+          Warn message!
+        </Toast>
+      )}
     </>
   )
 }
