2cd25cbd9ca687fc68320c64349be5e1efdb82b0
Jared White <jared@whitefusion.studio>
2023-03-28 08:38:56 -0700

    feat: new FieldValue component (#14)

---
+++ src/forms/FieldValue.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+
+import "./FieldValue.scss"
+
+export interface FieldValueProps {
+  /** Label content */
+  label?: string
+  /** Value content */
+  children: React.ReactNode
+  /** Additional help text below the content */
+  helpText?: string
+  /** Element ID */
+  id?: string
+  /** Additional CSS classes */
+  className?: string
+}
+
+const FieldValue = (props: FieldValueProps) => {
+  const classNames = ["field-value"]
+  if (props.className) classNames.push(props.className)
+
+  return (
+    <div id={props.id} className={classNames.join(" ")} role="gridcell">
+      {props.label && <p data-part="label">{props.label}</p>}
+      <p data-part="value">{props.children}</p>
+      {props.helpText && <p data-part="help-text">{props.helpText}</p>}
+    </div>
+  )
+}
+
+export default FieldValue
---
+++ src/forms/__stories_/FieldValue.stories.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import { Story } from "@storybook/react"
+import FieldValue from "../FieldValue"
+
+import MDXDocs from "./FieldValue.docs.mdx"
+
+export default {
+  title: "Forms/FieldValue",
+  component: FieldValue,
+  parameters: {
+    docs: {
+      page: MDXDocs,
+    },
+  },
+}
+
+export const standalone = () => <FieldValue>Pool, BBQ, Rooftop View</FieldValue>
+
+export const withLabel = () => (
+  <FieldValue label="Property Amenities">Pool, BBQ, Rooftop View</FieldValue>
+)
+
+export const withHelperText = () => (
+  <FieldValue label="Property Amenities" helpText="All good stuff">
+    Pool, BBQ, Rooftop View
+  </FieldValue>
+)
+
+export const inAGrid = () => (
+  <div role="grid" style={{maxWidth: "var(--bloom-width-4xl)"}}>
+    <div role="row" style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr"}}>
+      <FieldValue label="Property Amenities" helpText="All good stuff">
+        Pool, BBQ, Rooftop View
+      </FieldValue>
+      <FieldValue label="Property Amenities" helpText="All good stuff">
+        Pool, BBQ, Rooftop View
+      </FieldValue>
+      <FieldValue label="Property Amenities" helpText="All good stuff">
+        Pool, BBQ, Rooftop View
+      </FieldValue>
+    </div>
+  </div>
+)
