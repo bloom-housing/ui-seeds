a2147ae54d31a0236b51ba3784ccca4f02ed9b66
Jared White <jared@jaredwhite.com>
2023-04-17 18:29:20 -0700

    chore: add additional exports to index

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 export * from "./src/expandableText/ExpandableText"
 
-export { default as FormErrorMessage } from "./src/forms/FormErrorMessage"
 export { default as Alert } from "./src/blocks/Alert"
 export { default as Message } from "./src/blocks/Message"
 export { default as Toast } from "./src/blocks/Toast"
+export { default as Card } from "./src/blocks/Card"
+export { default as FieldValue } from "./src/forms/FieldValue"
+export { default as FormErrorMessage } from "./src/forms/FormErrorMessage"
+export { default as Icon } from "./src/icons/Icon"
