026a7e2c231e896895318f38e9a9ae2f435b8d1e
Jared White <jared@whitefusion.studio>
2023-06-19 18:23:29 -0700

    chore: add screen size variables and fix lg size (#41)

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   plugins: {
+    "@csstools/postcss-global-data": {
+      files: [
+        "src/global/tokens/screens.scss"
+      ]
+    },
+    "postcss-custom-media": {},
     autoprefixer: {},
   },
 }
