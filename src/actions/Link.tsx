8e7110996172b6a1778a1a15d75d9594056e9946
Jared White <jared@jaredwhite.com>
2023-06-28 21:06:17 -0700

    feat: add Link component

---
+++ src/actions/Link.tsx
@@ -0,0 +1,72 @@
+import React, { useContext } from "react"
+import { NavigationContext, isExternalLink, shouldShowExternalLinkIcon } from "../global/NavigationContext"
+
+import Icon from "../icons/Icon"
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
+
+import "./Link.scss"
+
+export interface LinkProps {
+  /** Link content */
+  children: React.ReactNode
+  /** URL to link to */
+  href?: string
+  /** Icon to show before the label text */
+  leadIcon?: React.ReactNode
+  /** Icon to show after the label text */
+  tailIcon?: React.ReactNode
+  /** Set to true if you don't want external links to show a related icon */
+  hideExternalLinkIcon?: boolean
+  /** Set to true to disable the button */
+  disabled?: boolean
+  /** Set to true to hide the link from the accessibility tree */
+  ariaHidden?: boolean
+  /** Accessible label if link doesn't contain text content */
+  ariaLabel?: string
+  /** Element ID */
+  id?: string
+  /** Additional CSS classes */
+  className?: string
+}
+
+const Link = (props: LinkProps) => {
+  const classNames = ["seeds-link"]
+  if (props.className) classNames.push(props.className)
+
+  const tailIcon = shouldShowExternalLinkIcon(props) ? (
+    <Icon icon={faArrowUpRightFromSquare} />
+  ) : (
+    props.tailIcon
+  )
+
+  if (props.leadIcon) classNames.push("has-lead-icon")
+  if (tailIcon) classNames.push("has-tail-icon")
+
+  const additionalProps = {
+    id: props.id,
+    className: classNames.join(" "),
+    "aria-label": props.ariaLabel,
+    "aria-hidden": props.ariaHidden,
+  }
+
+  if (props.href && isExternalLink(props.href)) {
+    return (
+      <a href={props.href} target="_blank" {...additionalProps}>
+        {props.leadIcon}
+        {props.children}
+        {tailIcon}
+      </a>
+    )
+  } else {
+    const { LinkComponent } = useContext(NavigationContext)
+    return (
+      <LinkComponent href={props.href} {...additionalProps}>
+        {props.leadIcon}
+        {props.children}
+        {tailIcon}
+      </LinkComponent>
+    )
+  }
+}
+
+export default Link
---
+++ src/actions/__stories__/Link.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+import Link from "../Link"
+
+import Icon from "../../icons/Icon"
+import { faHeart } from "@fortawesome/free-solid-svg-icons"
+
+//import MDXDocs from "./Button.docs.mdx"
+
+export default {
+  title: "Actions/Link",
+  component: Link,
+  // parameters: {
+  //   docs: {
+  //     page: MDXDocs,
+  //   },
+  // },
+}
+
+export const basicLink = () => <p>Hello world. <Link href="#">This is a link.</Link></p>
+
+export const externalLink = () => (
+  <div style={{ display: "flex", gap: "1rem" }}>
+    <Link href="/test">Internal Link</Link>
+    <Link href="https://www.exygy.com">External Link</Link>
+  </div>
+)
+
+export const linksWithIcons = () => (
+  <div style={{ display: "flex", gap: "1rem" }}>
+    <Link leadIcon={<Icon icon={faHeart} />}>Lead Icon</Link>
+    <Link tailIcon={<Icon icon={faHeart} />}>Tail Icon</Link>
+  </div>
+)
+
