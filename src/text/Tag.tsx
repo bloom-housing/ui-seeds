9ad9532218119b87aa9e0b1da2550d5bffde5d61
Jared White <jared@jaredwhite.com>
2023-07-11 20:08:35 -0700

    feat: continue building out Tag

--- src/text/Tag.tsx
+++ src/text/Tag.tsx
@@ -1,8 +1,31 @@
 import React from "react"
 
+import "./Tag.scss"
+
 export interface TagProps {
-  className?: string
+  /** Tag text */
   children: React.ReactNode
+  /** Appearance of the component */
+  variant?:
+    | "primary"
+    | "primary-inverse"
+    | "secondary"
+    | "secondary-inverse"
+    | "success"
+    | "success-inverse"
+    | "in-process"
+    | "in-process-inverse"
+    | "highlight-cool"
+    | "highlight-cool-inverse"
+    | "highlight-warm"
+    | "highlight-warm-inverse"
+  /** Tag size */
+  size?: "md" | "lg"
+  /** Element ID */
+  id?: string
+  /** Additional CSS classes */
+  className?: string
+  /** Additional ARIA label */
   ariaLabel?: string
 }
 
@@ -12,8 +35,15 @@ export const Tag = (props: TagProps) => {
   if (props.className) classNames.push(props.className)
 
   return (
-    <span className={classNames.join(" ")} aria-label={props.ariaLabel}>
+    <span
+      className={classNames.join(" ")}
+      data-variant={props.variant || "primary"}
+      data-size={props.size}
+      aria-label={props.ariaLabel}
+    >
       {props.children}
     </span>
   )
 }
+
+export default Tag
---
+++ src/text/__stories__/Tag.stories.tsx
@@ -0,0 +1,98 @@
+import React from "react"
+
+import Tag from "../Tag"
+
+import Icon from "../../icons/Icon"
+import { faFaceSmile } from "@fortawesome/free-solid-svg-icons"
+
+//import MDXDocs from "./Button.docs.mdx"
+import HeadingGroup from "../HeadingGroup"
+
+export default {
+  title: "Text/Tag",
+  component: Tag,
+  // parameters: {
+  //   docs: {
+  //     page: MDXDocs,
+  //   },
+  // },
+}
+
+export const defaultTag = () => <Tag>Default Tag</Tag>
+
+export const tagVariants = () => (
+  <>
+    <HeadingGroup size="2xl" heading="md" subheading="Regular Size" />
+    <div style={{ marginTop: "1rem" }}>
+      <Tag variant="primary">Primary Tag</Tag>
+      <Tag variant="secondary">Secondary Tag</Tag>
+      <Tag variant="success">Success Tag</Tag>
+      <Tag variant="in-process">In Process Tag</Tag>
+      <Tag variant="highlight-cool">Highlight Cool Tag</Tag>
+      <Tag variant="highlight-warm">Highlight Warm Tag</Tag>
+    </div>
+    <div style={{ marginBottom: "2rem" }}>
+      <Tag variant="primary-inverse">Primary Tag</Tag>
+      <Tag variant="secondary-inverse">Secondary Tag</Tag>
+      <Tag variant="success-inverse">Success Tag</Tag>
+      <Tag variant="in-process-inverse">In Process Tag</Tag>
+      <Tag variant="highlight-cool-inverse">Highlight Cool Tag</Tag>
+      <Tag variant="highlight-warm-inverse">Highlight Warm Tag</Tag>
+    </div>
+    <HeadingGroup size="2xl" heading="lg" subheading="Large Size" />
+    <div style={{ marginTop: "1rem" }}>
+      <Tag variant="primary" size="lg">
+        Primary Tag
+      </Tag>
+      <Tag variant="secondary" size="lg">
+        Secondary Tag
+      </Tag>
+      <Tag variant="success" size="lg">
+        Success Tag
+      </Tag>
+      <Tag variant="in-process" size="lg">
+        In Process Tag
+      </Tag>
+      <Tag variant="highlight-cool" size="lg">
+        Highlight Cool Tag
+      </Tag>
+      <Tag variant="highlight-warm" size="lg">
+        Highlight Warm Tag
+      </Tag>
+    </div>
+    <div style={{ marginBottom: "2rem" }}>
+      <Tag variant="primary-inverse" size="lg">
+        Primary Tag
+      </Tag>
+      <Tag variant="secondary-inverse" size="lg">
+        Secondary Tag
+      </Tag>
+      <Tag variant="success-inverse" size="lg">
+        Success Tag
+      </Tag>
+      <Tag variant="in-process-inverse" size="lg">
+        In Process Tag
+      </Tag>
+      <Tag variant="highlight-cool-inverse" size="lg">
+        Highlight Cool Tag
+      </Tag>
+      <Tag variant="highlight-warm-inverse" size="lg">
+        Highlight Warm Tag
+      </Tag>
+    </div>
+    <style>{".seeds-tag { margin: .25rem }"}</style>
+  </>
+)
+
+export const withIcons = () => (
+  <>
+    <div style={{ display: "flex", gap: ".5rem" }}>
+      <Tag>
+        <Icon icon={faFaceSmile} /> Icon Left
+      </Tag>
+      <Tag>
+        Icon Right <Icon icon={faFaceSmile} />
+      </Tag>
+    </div>
+  </>
+)
