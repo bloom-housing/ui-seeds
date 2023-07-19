e8428f5aaa7c5d954519e0146d95503d6532c608
Jared White <jared@jaredwhite.com>
2023-07-18 17:16:14 -0700

    tests: simplify Tag test

--- src/text/__tests__/Tag.test.tsx
+++ src/text/__tests__/Tag.test.tsx
@@ -12,14 +12,4 @@ describe("<Tag>", () => {
     expect(container.querySelector("#test-tag")).not.toBeNull()
     expect(container.querySelector("#test-tag.test-class")).not.toBeNull()
   })
-
-  it("supports variants", () => {
-    const { container } = render(<Tag variant="success-inverse">size lg</Tag>)
-    expect(container.querySelector(".seeds-tag[data-variant='success-inverse']")).not.toBeNull()
-  })
-
-  it("supports sizes", () => {
-    const { container } = render(<Tag size="lg">size lg</Tag>)
-    expect(container.querySelector(".seeds-tag[data-size='lg']")).not.toBeNull()
-  })
 })
