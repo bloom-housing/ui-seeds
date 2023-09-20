5d79b51b19a87456dbb53f6d85f6feb294e009e9
Jared White <jared@jaredwhite.com>
2023-09-20 15:35:17 -0700

    fix: drawer styling breakpoints docs: describe what the `usePortal` hook does

--- src/hooks/usePortal.ts
+++ src/hooks/usePortal.ts
@@ -1,6 +1,12 @@
 import React, { useCallback, useEffect, useRef, useState } from "react"
 
 /**
+ * This hook lets components "escape" out of their position in the DOM and be
+ * rendered at the `<body>` level (important for overlay-style components like
+ * dialogs and toasts). It also handles ARIA live announcement when required,
+ * and fires an event any time the ESC key is pressed to close the last
+ * element in a portal stack (useful for nested drawers and, again, toasts).
+ * 
  * @param portalId - unique id to represent the portal element (as child of `<body>`)
  * @param closeFn - function to call when ESC key is handled
  * @param liveAndAtomic - set to true for ARIA announcements
